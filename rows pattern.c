2b750bab850eacdbb9b801f5268afdd167a3aa18
kankalapooja16 <kankalapooja16@gmail.com>
2026-02-04 12:40:15 +0530

    Add files via upload

---
+++ rows pattern.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    printf("enter no of rows \n");
+    scanf("%d",&rows);
+    for(int i=1;i<=rows;i++)
+    {
+        printf("* \n");
+    }
+}
