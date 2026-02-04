410ac7b77f97368e2286e71e9a89b95b05407fe0
kankalapooja16 <kankalapooja16@gmail.com>
2026-02-04 12:42:03 +0530

    Add files via upload

---
+++ left triangle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    int columns;
+    printf("enter no of rows \n");
+    scanf("%d",&rows);
+    printf("enter no of columns \n");
+    scanf("%d",&columns);
+    for(int i=1;i<=rows;i++)
+    {
+         for(int j=1;j<=columns;j++)
+{
+    if(i==rows||j==columns||i+j>=(rows+1))
+{
+    printf("* ");
+}
+    else
+{
+    printf("  ");
+}
+}
+    printf("\n");
+}
+}
